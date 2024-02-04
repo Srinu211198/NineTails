70f096469f2cae8d736b56aa489bce6c4c590920
Srinu211198 <sn20acy@gmail.com>
2024-02-04 17:26:24 +0530

    Adding basic java files

---
+++ Test.java
@@ -0,0 +1,15 @@
+class Test
+{
+    public static void main(String[] args)
+    {
+        // This is single line comment
+        System.out.println("This program explains the different Comments in JAVA"); // Comment at end of statement
+        /*
+         This 
+         is 
+         a 
+         multi-line 
+         comment
+         */
+    }
+}
---
+++ Test2.java
@@ -0,0 +1,11 @@
+public class Test2 
+{
+    public static void main(String[] args) 
+       {
+            int i = 10;
+            float f =12.35f;
+            double d =1232.45657;
+            String s = "Hello this is a string";
+     
+       }
+}
