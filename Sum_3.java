8eafd0426676c25178f323289ac8139be987ac4f
Srinu211198 <sn20acy@gmail.com>
2024-02-07 01:22:52 +0530

    sum of three no.

---
+++ Sum_3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Sum_3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the numbers:");
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println("The sum of three numbers is "+(a+b+c));
+
+    }
+    
+}
