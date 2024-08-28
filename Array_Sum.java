14a5028e0c87fbc2cd1b9cfe54f39fb2563e0fcf
Srinu211198 <94193956+Srinu211198@users.noreply.github.com>
2024-08-28 23:31:05 +0100

    G

---
+++ Array_Sum.java
@@ -0,0 +1,12 @@
+public class Array_Sum {
+    public static void main(String[] args) {
+        int a[]={2,4,6,7,9};
+        int sum =0;
+
+        for(int n:a){
+            sum +=n;
+        }
+        System.out.println("sum ="+sum);
+    }
+    
+}
