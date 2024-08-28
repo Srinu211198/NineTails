53e2a265e052ec58553f6fb6bd46ea735d97924d
Srinu211198 <94193956+Srinu211198@users.noreply.github.com>
2024-08-28 23:32:26 +0100

    GFG Basic

--- Hello.java
+++ Hello.java
@@ -1,10 +1,8 @@
-/**
- * Hello
- */
-public class Hello {
+class Hello{
+public static void main(String args[]){
 
-    public static void main(String[] args) {
-        
-        System.out.println("Hello");
-    }
+    System.out.println("Hello World!");
+
+}
+
 }
